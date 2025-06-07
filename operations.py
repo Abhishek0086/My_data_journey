53d703210c6500c5462de0daad33d9242662638f
Abhishek0086 <130195535+Abhishek0086@users.noreply.github.com>
2025-06-07 11:32:35 +0530

    Added(operations.py):learnt how to format string

---
+++ operations.py
@@ -0,0 +1,15 @@
+age = 21
+print(f"My age is {age}")
+
+""" name = "lisa"
+greeting = f"hello {name}"
+print(greeting) """
+
+name = "lisa"
+final_greeting =" hello {}"
+lisa_greeting = final_greeting.format(name)
+print(lisa_greeting)
+
+name = "Aren"
+aren_greeting = final_greeting.format(name)
+print(aren_greeting)
