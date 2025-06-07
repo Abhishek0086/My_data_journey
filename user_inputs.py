1128255931a30d78d872ba37057ac4b1d3b2eb00
Abhishek0086 <130195535+Abhishek0086@users.noreply.github.com>
2025-06-07 11:48:35 +0530

    Added(user_inputs):learnt about string multiplications

---
+++ user_inputs.py
@@ -0,0 +1,8 @@
+""" my_name = "lisa"
+your_name = input("Enter your name:")
+print(f"Hello, {your_name}. My name is {my_name}")
+ """
+
+age = int(input("Enter your age :"))
+months = age * 12
+print(f"you have lived for {months} months ")
