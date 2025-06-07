ed00f42444db9a12aa7726cd9f7a74f77da9ba2e
Abhishek0086 <130195535+Abhishek0086@users.noreply.github.com>
2025-06-07 11:19:41 +0530

    Added(texts.py):worked on strings

---
+++ texts.py
@@ -0,0 +1,17 @@
+my_string = 'Hello,World!'
+
+escaped_string = "hello miss \" lisa\"" # you put an escape character before the double quotes 
+
+multiline_string = """Hello,
+lisa"""
+
+print(multiline_string)
+
+name = "lisa"
+greeting = "hello"
+
+print(greeting+"  "+name)
+
+age = 21
+age_as_string = str(age)
+print("my age is " + age_as_string)
