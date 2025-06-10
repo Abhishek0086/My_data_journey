d870825540a07795a21315fc28f4e1aff1e72bbb
Abhishek0086 <130195535+Abhishek0086@users.noreply.github.com>
2025-06-10 22:42:10 +0530

    Updated(sets.py):learnt about sets

--- sets.py
+++ sets.py
@@ -8,3 +8,15 @@ print(art_friends) """
 
 art_but_notscience = art_friends.difference(science_friends)
 print(art_but_notscience)
+science_but_notart = science_friends.difference(art_friends)
+print(science_but_notart)
+
+
+not_in_both = art_friends.symmetric_difference(science_friends)
+print(not_in_both)
+
+friends_in_both = art_friends.intersection(science_friends)
+print(friends_in_both)
+
+friends_in_either = art_friends.union(science_friends)
+print(friends_in_either)
