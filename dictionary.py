aca002476b376001f3d28815631a253a2dfe3959
Abhishek0086 <130195535+Abhishek0086@users.noreply.github.com>
2025-06-11 14:56:52 +0530

    UPDATED(dictionary):learnt more about dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,2 +1,17 @@
 dictionary = {"name": "pluto", "type": "dog", "age": 5, "color": "brown"}
 print(dictionary)
+
+print(dictionary["type"])
+
+dictionary["age"] = 6 # Update the age of the dog
+print(dictionary)
+
+collection = (
+    {"name":"pluto"},
+    {"name":"lisa"},
+    {"name":"charlie"},
+    {"name":"bob"},
+    {"name":"jen"}
+)
+
+print(collection[0]["name"])
