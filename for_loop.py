0e9c72806ed82f579e6cf14dbe6cc8c29773451e
Abhishek0086 <130195535+Abhishek0086@users.noreply.github.com>
2025-06-13 09:06:57 +0530

    ADDED(for_loops.py):learnt for loop

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+friends = ['Rolf', 'Anne', 'Charlie']
+
+for friend in friends:
+    print(f'Hello, {friend}!')
+
+elements = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for index in elements:
+    print(index)
+
+
+for index in range(len(elements)):
+    print(f'Index {index} has value {elements[index]}')
