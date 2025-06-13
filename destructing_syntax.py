08e27b194f59bc6a67ef5af44090b9183a5f4820
Abhishek0086 <130195535+Abhishek0086@users.noreply.github.com>
2025-06-13 09:41:06 +0530

    UPDATED(destructing_syntax.py): values & item function for dictionary

--- destructing_syntax.py
+++ destructing_syntax.py
@@ -7,3 +7,9 @@ friends = [('Rolf',25), ('Anne',23), ('Charlie',30)]
 for name, age in friends:
     print(f'{name} is {age} years old')
 
+friend_ages = {'rolf':25, 'anne':23, 'charlie':30}
+
+for age in friend_ages.values():
+    print(age)
+for name,age in friend_ages.items():
+    print(f'{name} is {age} years old')
