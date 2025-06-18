eca79a021011c62b0b79c2b139a0b5e0f76d42cf
Abhishek0086 <130195535+Abhishek0086@users.noreply.github.com>
2025-06-18 15:12:08 +0530

    ADDED(enumerate.py):learnt about enumerate function

---
+++ enumerate.py
@@ -0,0 +1,13 @@
+friends =  ['Rolf', 'ruth', 'Charlie', 'jen']
+""" index = 0
+for freinds in friends:
+    print(f"{index} {freinds}")
+    index += 1 """
+
+counter = 0
+
+for counter,friend in enumerate(friends,start = 1):
+    print(f"{counter} {friend}")
+
+print(list(enumerate(friends)))
+print(dict(enumerate(friends)))
