16fd901cbea53eee3bf803eca59d00a5db230796
Abhishek0086 <130195535+Abhishek0086@users.noreply.github.com>
2025-07-12 11:06:46 +0530

    ADDED(inheritance.py): learnt about inheritance

--- inheritance.py
+++ inheritance.py
@@ -5,3 +5,18 @@ class Student:
         self.marks = []
     def average(self):
         return sum(self.marks) / len(self.marks) if self.marks else 0
+
+class WorkingStudent(Student):
+    def __init__(self, name,school, salary):
+        super().__init__(name,school)
+        self.salary = salary
+
+
+rolf = WorkingStudent('Rolf', 'mit', 1000)
+print(rolf.salary)
+print(rolf.name)
+print(rolf.school)
+
+rolf.marks.append(90)
+rolf.marks.append(88)
+print(rolf.average())
